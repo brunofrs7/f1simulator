c76dcaaa84dfdb209cc186ca569faae8dc74bc09
Bruno <brunofrs7@gmail.com>
2020-02-15 00:12:41 +0000

    SplashScreen terminado

---
+++ app/src/main/java/com/example/f1simulator/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.f1simulator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        }, 3000);
+
+        MediaPlayer audio = MediaPlayer.create(SplashScreenActivity.this, R.raw.intro);
+        audio.start();
+    }
+}
