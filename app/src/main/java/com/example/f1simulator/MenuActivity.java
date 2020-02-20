4014d6200b9fbc9fe2424715c4ba894ac1438fde
Bruno <brunofrs7@gmail.com>
2020-02-20 22:10:30 +0000

    Menu completo Icone e nome da APP alterados Tradução para Inglês

--- app/src/main/java/com/example/f1simulator/MenuActivity.java
+++ app/src/main/java/com/example/f1simulator/MenuActivity.java
@@ -3,12 +3,50 @@ package com.example.f1simulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        mViewHolder.bt_menu_circuito = findViewById(R.id.bt_menu_circuito);
+        mViewHolder.bt_menu_piloto = findViewById(R.id.bt_menu_piloto);
+        mViewHolder.bt_menu_equipa = findViewById(R.id.bt_menu_equipa);
+        mViewHolder.bt_menu_simulador = findViewById(R.id.bt_menu_simulador);
+
+        mViewHolder.bt_menu_circuito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mViewHolder.bt_menu_piloto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mViewHolder.bt_menu_equipa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mViewHolder.bt_menu_simulador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private class ViewHolder {
+        Button bt_menu_piloto, bt_menu_circuito, bt_menu_equipa, bt_menu_simulador;
     }
 }
